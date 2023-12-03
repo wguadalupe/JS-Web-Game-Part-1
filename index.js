6a4396fdfa7e163345bd9720258dfafe4c6816fb
wguadalupe <w.guadalupe78@gmail.com>
2023-12-03 09:51:03 -0500

    added images

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(src, left, bottom) {
+    let image = document.createElement('img');
+    image.src = src;
+    image.style.position = 'fixed';
+    image.style.left = left;
+    image.style.bottom = bottom;
+    document.body.append(image);
+}
+
+newImage('assets/green-character.gif', '100px', '100px');
+newImage('assets/pine-tree.png', '450px', '200px');
+newImage('assets/tree.png', '200px', '300px');
+newImage('assets/pillar.png', '350px', '100px'); 
+newImage('assets/crate.png', '150px', '200px');
+newImage('assets/well.png', '500px', '425px');
+
+let sword = document.createElement('img');
+sword.src = 'assets/sword.png';
+sword.style.position = 'fixed';  
+sword.style.left = '500px';      
+sword.style.bottom = '405px';    
+document.body.append(sword);
+
+sword.addEventListener('click', function() {
+    sword.remove();
+});
